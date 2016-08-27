ef90fc1f4d2ed7122d1538cac3d9049bb7ac20f3
Ashley <ashleymsutton@outlook.com>
2016-08-27 17:28:42 -0400

    added routes for fb auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'sessions/create'
-
   get 'sessions/destroy'
+
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
